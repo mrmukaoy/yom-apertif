66f93097e1a5f41e0cfc3d7eda86fd9c44e6b77b
Martin Yoakum <MYOAKUM@WUSTL.EDU>
2021-03-16 17:07:24 -0500

    add/install composer & node.js

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd3f76149f5ea2e7cda63c3a1b1ff2f8aab24910d',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd3f76149f5ea2e7cda63c3a1b1ff2f8aab24910d',
+    ),
+    'monolog/monolog' => 
+    array (
+      'pretty_version' => '1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+    ),
+  ),
+);
