8a787dc0904d550a49917cb51de022535836ff80
Martin Yoakum <MYOAKUM@WUSTL.EDU>
2021-03-17 00:06:23 -0500

    reorganize file structure

--- functions.php
+++ functions.php
@@ -143,7 +143,7 @@ function apertif_scripts() {
 	wp_enqueue_style( 'apertif-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'apertif-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'apertif-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'apertif-navigation', get_template_directory_uri() . '/_assets/js/navigation.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -154,33 +154,33 @@ add_action( 'wp_enqueue_scripts', 'apertif_scripts' );
 /**
  * Implement the Custom Header feature.
  */
-require get_template_directory() . '/inc/custom-header.php';
+require get_template_directory() . '/_inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
  */
-require get_template_directory() . '/inc/template-tags.php';
+require get_template_directory() . '/_inc/template-tags.php';
 
 /**
  * Functions which enhance the theme by hooking into WordPress.
  */
-require get_template_directory() . '/inc/template-functions.php';
+require get_template_directory() . '/_inc/template-functions.php';
 
 /**
  * Customizer additions.
  */
-require get_template_directory() . '/inc/customizer.php';
+require get_template_directory() . '/_inc/customizer.php';
 
 /**
  * Load Jetpack compatibility file.
  */
 if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
+	require get_template_directory() . '/_inc/jetpack.php';
 }
 
 /**
  * Load WooCommerce compatibility file.
  */
 if ( class_exists( 'WooCommerce' ) ) {
-	require get_template_directory() . '/inc/woocommerce.php';
+	require get_template_directory() . '/_inc/woocommerce.php';
 }
